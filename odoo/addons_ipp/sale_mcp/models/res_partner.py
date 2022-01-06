1da8aa7c67944aaa901572e60390a626668b4805
quanvm <quanvm00@gmail.com>
2022-01-07 05:32:57 +0700

    1

--- odoo/addons_ipp/sale_mcp/models/res_partner.py
+++ odoo/addons_ipp/sale_mcp/models/res_partner.py
@@ -7,6 +7,7 @@ class Respartner(models.Model):
     _inherit = 'res.partner'
 
     saleperson_ids = fields.One2many('res.partner.saleperson', 'partner_id', string='Salepersons')
+    default_code = fields.Char("Customer Code")
 
     def gen_mcp(self):
         mcp_obj = self.env['mcp.mcp'].sudo()
