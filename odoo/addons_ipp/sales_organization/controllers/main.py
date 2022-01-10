e154f8f79fc407071e55991659a1cad1bf038e68
quanvm <quanvm00@gmail.com>
2022-01-10 10:26:24 +0700

    1

--- odoo/addons_ipp/sales_organization/controllers/main.py
+++ odoo/addons_ipp/sales_organization/controllers/main.py
@@ -24,15 +24,16 @@ class APIController(APIController):
                 lst_sol = []
                 for line in so.order_line:
                     line_val = {
-                        'id': line.id,
+                        # 'id': line.id,
                         'name': line.name or '',
-                        'product_id': line.product_id.id or False,
+                        # 'product_id': line.product_id.id or False,
                         'product_name': line.product_id.name or '',
+                        'product_code': line.product_id.default_code or '',
 
                         'price_unit': line.price_unit,
                         'discount': line.discount,
                         'product_qty': line.product_uom_qty,
-                        'product_uom_id': line.product_uom and line.product_uom.id or False,
+                        # 'product_uom_id': line.product_uom and line.product_uom.id or False,
                         'product_uom': line.product_uom and line.product_uom.name or '',
 
                         'qty_delivered': line.qty_delivered,
@@ -44,23 +45,27 @@ class APIController(APIController):
                     lst_sol.append(line_val)
 
                 so_val = {
-                    'user_id': so.user_id and so.user_id.id or False,
-                    'partner_id': so.partner_id.id,
+                    # 'user_id': so.user_id and so.user_id.id or False,
+                    # 'partner_id': so.partner_id.id,
                     'partner_name': so.partner_id.name or '',
                     'partner_code': so.partner_id.default_code or '',
-                    'partner_invoice_id': so.partner_invoice_id and so.partner_invoice_id.id or False,
-                    'partner_invoice_name': so.partner_invoice_id and so.partner_invoice_id.name or '',
-                    'partner_shipping_id': so.partner_invoice_id and so.partner_shipping_id.id or False,
-                    'partner_shipping_name': so.partner_invoice_id and so.partner_shipping_id.name or '',
-                    'channel': '',
+                    # 'partner_invoice_id': so.partner_invoice_id and so.partner_invoice_id.id or False,
+                    'partner_invoice_name': so.partner_invoice_id and so.partner_invoice_id.contact_address or '',
+                    # 'partner_shipping_id': so.partner_invoice_id and so.partner_shipping_id.id or False,
+                    'partner_shipping_name': so.partner_invoice_id and so.partner_shipping_id.contact_address or '',
+                    # 'channel': '',
                     'code': so.name or '',
                     'create_date': so.create_date or '',
                     'date_order': so.date_order or '',
-                    'company_id': so.company_id and so.company_id.id or False,
+                    # 'company_id': so.company_id and so.company_id.id or False,
                     'company_name': so.company_id and so.company_id.name or False,
+                    'user_name': so.user_id and so.user_id.name or '',
                     'note': so.note,
-                    'status': so.state,
-                    'order_line_items': lst_sol
+                    'state': so.state,
+                    'order_line_items': lst_sol,
+                    'amount_untaxed':so.amount_untaxed,
+                    'amount_tax': so.amount_tax,
+                    'amount_total': so.amount_total,
                 }
                 res.append(so_val)
 
